cb8d5e2579185b29b87f61a03ecf30179efbaf5c
jamiegarner <jamiegarner@JAMIEGARNER86A4>
2016-11-24 17:01:33 +0000

    Add project files.

---
+++ PrelimWQ/Controllers/QuestionnaireController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrelimWQ.Controllers
+{
+    public class QuestionnaireController : Controller
+    {
+        // GET: Questionnaire
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
